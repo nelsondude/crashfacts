103d988907cbf133c63f41b6c0dc2d47ea915864
nelsondude <alexn1336@gmail.com>
2019-02-09 15:39:31 -0500

    Rotating logo on the size

--- src/containers/App.js
+++ src/containers/App.js
@@ -181,7 +181,7 @@ class App extends Component {
           <br/>
           <br/>
           <br/>
-          <div>
+          <div className={'rotate-360'}>
             <svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
               <ellipse cx="120" cy="120" rx="50" ry="100" stroke="red" fill="red" stroke-width="5" />
