078874b0183a57bbdb5491b414a9cdb5f2fc9f43
nelsondude <alexn1336@gmail.com>
2019-02-09 00:42:04 -0500

    Show pdf with good formatting

--- src/components/Lecture/Lecture.js
+++ src/components/Lecture/Lecture.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './Lecture.css';
 
 class Lecture extends Component {
+  state = {
+    file: null,
+    page: 1
+  };
+
+  constructor(props) {
+    super(props);
+    this.file = React.createRef();
+  }
+
+  handleChange = (event) => {
+    console.log(event.target.files[0]);
+    this.setState({file: URL.createObjectURL(event.target.files[0])});
+  };
+
+
   render() {
     return (
       <div className={'Lecture'}>
 
         <h1>Lecture</h1>
+        <input onChange={this.handleChange} type="file" ref={this.file}/>
+        {this.state.file ?
+          <object data={this.state.file + `#view=FitH&page=` + this.state.page} type="application/pdf">
+          <embed src={this.state.file + `#view=FitH&page=` + this.state.page} type="application/pdf" />
+        </object> : null}
 
       </div>
     )
